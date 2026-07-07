105f271b77a2eba7449bd79e586b7a301c17367f
Lê Hoài Ân <98139595+hoaianle@users.noreply.github.com>
2026-07-07 22:22:50 +0700

    feat(tools): add rotate log in postgres tool

--- tools/postgresql-backup/backup.sh
+++ tools/postgresql-backup/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+NUMBER_OF_LOG_TO_KEEP=5
 
 # Load configuration from an external file
 if [[ -z "${DB_HOST:-}" || -z "${DB_USER:-}" || -z "${DB_PASSWORD:-}" || -z "${DB_NAME:-}" ]]; then
@@ -19,7 +20,7 @@ dump_file="$ROOT_DIR/dumps/${DB_NAME}/${current_timestamp}.sql"
 
 mkdir -p "$ROOT_DIR/dumps/${DB_NAME}"
 
-main() {
+create_backup() {
     echo "Starting full backup for database '$DB_NAME'..."
     start_time=$(date +%s%3N)
 
@@ -30,6 +31,17 @@ main() {
 
     echo "Backup process finished in $duration ms."
     echo "File: $dump_file"
+}
+
+rotate_backups() {
+    ls -t1 "$ROOT_DIR/dumps/$DB_NAME" | tail -n +"$((NUMBER_OF_LOG_TO_KEEP+1))" | while IFS= read -r file; do
+        rm -- "$ROOT_DIR/dumps/$DB_NAME/$file"
+    done
+}
+
+main() {
+   create_backup
+   rotate_backups
 }
 
 main
